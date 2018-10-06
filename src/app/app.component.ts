12062d5570b734f0ffbf2ba65807461a8777b6ac
serpento <serpento@devadmin.com>
2018-10-06 15:57:35 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form13';
+export class AppComponent implements OnInit {
+
+  public myForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this._fb.group({
+      FrontEnd: ['',],
+      rooms: this._fb.array([
+        this.initRoomInput(),
+      ])
+    });
+  }
+
+  initRoomInput() {
+    return this._fb.group({
+      room: ['']
+    });
+  }
+
+  addRoom() {
+    const control = <FormArray>this.myForm.controls['rooms'];
+    control.push(this.initRoomInput());
+
+  }
+
+  removeRoom(i: number) {
+    const control = <FormArray>this.myForm.controls['rooms'];
+    control.removeAt(i);
+  }
+
+  save(model) {
+    console.log(model);
+  }
+
 }
